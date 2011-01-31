2fd2400f385944feaa7159d242da89b1669316d2
ajshort <andrewjshort@gmail.com>
2011-01-31 15:32:16 +1100

    Added the config file, with code to add the XML-RPC library to the include path.

---
+++ _config.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @package silverstripe-wordpressconnector
+ */
+
+// Add the Zend XML-RPC library to the include path.
+set_include_path(
+	dirname(__FILE__) . '/thirdparty' . PATH_SEPARATOR . get_include_path());
