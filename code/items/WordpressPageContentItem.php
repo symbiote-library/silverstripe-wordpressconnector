f681ca695c2427df978ed44c39a98cfe26133f58
ajshort <andrewjshort@gmail.com>
2011-01-31 16:13:22 +1100

    Added the basic wordpress page source and item connectors.

---
+++ code/items/WordpressPageContentItem.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * A content item that represents a wordpress page.
+ *
+ * @package silverstripe-wordpressconnector
+ */
+class WordpressPageContentItem extends ExternalContentItem {
+
+	/**
+	 * @param  array $data
+	 * @return WordpressPageContentItem
+	 */
+	public static function factory($source, $data) {
+		$item = new self($source, $data['page_id']);
+
+		// If page_title is set, then this is just a partial data chunk.
+		if (isset($data['page_title'])) {
+			$item->Title     = $data['page_title'];
+			$item->ParentID  = $data['page_parent_id'];
+			$item->CreatedAt = strtotime($data['dateCreated']);
+		} else {
+			$item->CreatedAt     = strtotime($data['dateCreated']);
+			$item->UserID        = $data['userid'];
+			$item->Status        = $data['page_status'];
+			$item->Description   = $data['description'];
+			$item->Title         = $data['title'];
+			$item->Link          = $data['link'];
+			$item->Permalink     = $data['permaLink'];
+			$item->Excerpt       = $data['excerpt'];
+			$item->TextMore      = $data['text_more'];
+			$item->AllowComments = $data['mt_allow_comments'];
+			$item->AllowPings    = $data['mt_allow_pings'];
+			$item->Slug          = $data['wp_slug'];
+			$item->Password      = $data['wp_password'];
+			$item->Author        = $data['wp_author'];
+			$item->ParentID      = $data['wp_page_parent_id'];
+			$item->ParentTitle   = $data['wp_page_parent_title'];
+			$item->Order         = $data['wp_page_order'];
+			$item->AuthorID      = $data['wp_author_id'];
+			$item->AuthorName    = $data['wp_author_display_name'];
+			$item->Template      = $data['wp_page_template'];
+		}
+
+		return $item;
+	}
+
+	public function getCMSFields() {
+		$fields = parent::getCMSFields();
+
+		$fields->fieldByName('Root.Details')->getChildren()->changeFieldOrder(array(
+			'Title', 'Status', 'CreatedAt', 'ShowContentInMenu', 'Description',
+			'Excerpt', 'TextMore', 'ExternalContentItem_Alert'
+		));
+
+		$fields->addFieldsToTab('Root.Users', array(
+			new ReadonlyField('UserID', 'User ID', $this->UserID),
+			new ReadonlyField('Author', null, $this->Author),
+			new ReadonlyField('AuthorID', 'Author ID', $this->AuthorID),
+			new ReadonlyField('AuthorName', 'Author Name', $this->AuthorName)
+		));
+
+		$fields->addFieldsToTab('Root.Metadata', array(
+			new ReadonlyField('Slug', null, $this->Slug),
+			new ReadonlyField('Link', null, $this->Link),
+			new ReadonlyField('Permalink', null, $this->Permalink)
+		));
+
+		$fields->addFieldsToTab('Root.Behaviour', array(
+			new ReadonlyField('ParentID', 'Parent ID', $this->ParentID),
+			new ReadonlyField('ParentTitle', 'Parent Title', $this->ParentTitle),
+			new ReadonlyField('AllowComments', 'Allow Comments', $this->AllowComments),
+			new ReadonlyField('AllowPings', 'Allow Pings', $this->AllowPings),
+			new ReadonlyField('Password', null, $this->Password),
+			new ReadonlyField('Template', null, $this->Template),
+			new ReadonlyField('Order', null, $this->Order)
+		));
+
+		return $fields;
+	}
+
+	public function stageChildren() {
+		return $this->source->getPagesByParentId($this->externalId);
+	}
+
+}
---
+++ code/sources/WordpressPageContentSource.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * A content source that displays all the pages attached to a wordpress blog.
+ *
+ * @package silverstripe-wordpressconnector
+ */
+class WordpressPageContentSource extends WordpressContentSource {
+
+	public function getRoot() {
+		return $this;
+	}
+
+	public function getObject($id) {
+		$client = $this->getClient($id);
+		$id     = $this->decodeId($id);
+
+		$page = $client->call('wp.getPage', array(
+			$this->BlogId, $id, $this->Username, $this->Password
+		));
+
+		if ($page) {
+			return WordpressPageContentItem::factory($this, $page);
+		}
+	}
+
+	public function stageChildren() {
+		return $this->getPagesByParentId(0);
+	}
+
+	/**
+	 * Gets all the page content items that sit under a parent ID.
+	 *
+	 * @param  int $parent
+	 * @return DataObjectSet
+	 */
+	public function getPagesByParentId($parent) {
+		$result = new DataObjectSet();
+
+		if (!$this->isValid()) {
+			return $result;
+		}
+
+		try {
+			$client = $this->getClient();
+			$pages  = $client->call('wp.getPageList', array(
+				$this->BlogId, $this->Username, $this->Password
+			));
+		} catch (Zend_Exception $exception) {
+			SS_Log::log($exception, SS_Log::ERR);
+			return new DataObjectSet();
+		}
+
+		foreach ($pages as $page) {
+			if ($page['page_parent_id'] == $parent) {
+				$result->push(WordpressPageContentItem::factory($this, $page));
+			}
+		}
+
+		return $result;
+	}
+
+}
