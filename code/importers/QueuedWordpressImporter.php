cd2590a92abe4227143820981139785fd296c2b6
ajshort <andrewjshort@gmail.com>
2011-02-01 15:43:59 +1100

    Added a queued version of the wordpress importer.

---
+++ code/importers/QueuedWordpressImporter.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * A queued version of the wordpress importer.
+ *
+ * @package silverstripe-wordpressconnector
+ */
+class QueuedWordpressImporter extends QueuedExternalContentImporter {
+
+	public function init() {
+		$page = new WordpressPageTransformer();
+		$page->setImporter($this);
+
+		$post = new WordpressPostTransformer();
+		$post->setImporter($this);
+
+		$this->contentTransforms['page'] = $page;
+		$this->contentTransforms['post'] = $post;
+	}
+
+	public function getExternalType($item) {
+		switch ($item->class) {
+			case 'WordpressPageContentItem': return 'page';
+			case 'WordpressPostContentItem': return 'post';
+		}
+	}
+
+}
